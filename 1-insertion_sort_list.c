851f4daffb7f797d34cd5d285dbd3dfa302d61b5
Jasemonu <jasemonu@gmail.com>
2023-04-23 15:21:59 -0700

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,51 +1,52 @@
 #include "sort.h"
 
-void pos_node(listint_t **head, listint_t **node_1, listint_t *node_2);
-
 /**
- * insertion_sort_list - a function that sorts a doubly linked list
- *	of integers in ascending order using the Insertion sort algorithm
- *
- * @list: linked list of integers
- * Return: void
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
-void insertion_sort_list(listint_t **list)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	listint_t *temp, *unsorted, *sorted;
-
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-	for (unsorted = (*list)->next; unsorted != NULL; unsorted = temp)
-	{
-		temp = unsorted->next;
-		sorted = unsorted->prev;
-		while (sorted != NULL && unsorted->n < sorted->n)
-		{
-			pos_node(list, &sorted, unsorted);
-			print_list((const listint_t *)*list);
-		}
-	}
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
- * pos_node - Puts nodes in thier right position in a
- *	listint_t doubly-linked list.
- * @head: A points to the head of the doubly-linked list.
- * @node_1: First node to position.
- * @node_2: The second node to position.
- * Retrun: void
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
-void pos_node(listint_t **head, listint_t **node_1, listint_t *node_2)
+void insertion_sort_list(listint_t **list)
 {
-	(*node_1)->next = node_2->next;
-	if (node_2->next != NULL)
-		node_2->next->prev = *node_1;
-	node_2->prev = (*node_1)->prev;
-	node_2->next = *node_1;
-	if ((*node_1)->prev != NULL)
-		(*node_1)->prev->next = node_2;
-	else
-		*head = node_2;
-	(*node_1)->prev = node_2;
-	*node_1 = node_2->prev;
+	listint_t *new_node, *previous_node, *temp_node;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	/* Iterate over each node in the list. */
+	for (new_node = (*list)->next; new_node != NULL; new_node = temp_node)
+	{
+		temp_node = new_node->next; /* Store the next node to be visited. */
+		previous_node = new_node->prev; /* Start from the previous node. */
+
+		while (previous_node != NULL && new_node->n < previous_node->n)
+		{
+			swap_nodes(list, &previous_node, new_node);  /* Swap nodes if necessary. */
+			print_list((const listint_t *)*list);  /* Print the updated list. */
+			previous_node = new_node->prev;  /* Continue iterating backwards. */
+		}
+	}
 }
